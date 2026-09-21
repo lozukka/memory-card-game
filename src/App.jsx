dfe2106b195da26fd4f27ffcf657f23b39516cc7
Lotta <lotsulotsu@gmail.com>
2026-09-21 16:49:24 +0300

    learn how to use api and fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {}
+function App() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+      .then((response) => response.json())
+      .then((data) => {
+        setImage(data.sprites.front_default);
+      });
+  }, []);
+
+  return <>{image && <img src={image} alt="ditto" />}</>;
+}
 
 export default App;
