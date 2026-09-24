5270b0e7fc3854c55e2e62865c063e5979566758
Lotta <lotsulotsu@gmail.com>
2026-09-24 08:56:05 +0300

    styled components

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,8 +1,10 @@
 function Score({ score, bestScore }) {
   return (
     <>
-      <h2>Your score is: {score}</h2>
-      <h2>Your best score is: {bestScore}</h2>
+      <div>
+        <h2>Your score is: {score}</h2>
+        <h2>Your best score is: {bestScore}</h2>
+      </div>
     </>
   );
 }
